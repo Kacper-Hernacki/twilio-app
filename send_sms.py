87bed63e539f12de2a8e88b22a332aeb9ea30771
Kacper <hernackikacper@gmail.com>
2020-06-04 14:48:23 +0200

    first commit

---
+++ send_sms.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twilio
+
+client = Client(account_sid, auth_token)
+
+my_msg = 'Siema!'
+
+message = client.messages.create(to=my_cell, from_=my_twilio,
+                                 body=my_msg)
+
+
+
+
+*************************COMMANDS***************************************
+#GITbash command window
+#cd
+#ls
+#cd Desktop
+#cd Twilio\ APP
+#ls
+#python send_sms.py
